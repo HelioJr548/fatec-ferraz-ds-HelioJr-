a8fc1c53d3415e2219f91e8f2c9800ada56271a3
HelioJr <heliojr548@gmail.comm>
2022-10-19 09:16:00 -0300

    Preparando para outras pesquisas

--- SpringBoot/Aula01/crud/src/main/java/com/ds2022/crud/Repository/CourseRepository.java
+++ SpringBoot/Aula01/crud/src/main/java/com/ds2022/crud/Repository/CourseRepository.java
@@ -1,5 +1,6 @@
 package com.ds2022.crud.Repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,11 @@ import com.ds2022.crud.Model.Course;
 
 @Repository //Faz gravações e manipulação de dados do Banco de Dados
 public interface CourseRepository extends JpaRepository<Course, Long>{
-    
+    List<Course> findByName(String name);
+    List<Course> findByCategory(String category);
+    List<Course> findByNameStartingWith(String prefix);
+    List<Course> findByNameEndingWith(String suffix);
+    List<Course> findByNameContaining(String infix);
+    List<Course> findByIdBetween(Long startId, Long endId);
+    List<Course> findByNameLike(String likePattern);
 }
